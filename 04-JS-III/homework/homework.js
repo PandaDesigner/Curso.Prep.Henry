80bb348026c8c3243972dc1fab77f270ae947795
PandaDesigner <pedro@pandadesigners.com>
2021-10-29 22:35:59 -0500

    04-js

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -107,20 +107,42 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  let numeroMasGrande = numeros[0];
+  for (let i = 0; i< numeros.length; i++) {
+        if (numeroMasGrande<numeros[i]) {
+          numeroMasGrande = numeros[i];
+        }
+  } return numeroMasGrande;
 }
 
 
 function multiplicarArgumentos() {
-  // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
-  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
+  // Usa la palabra clave `arguments` -- para multiplicar todos los argumentos y devolver el producto
+  // Si no se pasan argumentos devuelve 0. 
+  //Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if ( arguments.length === 0) {
+    return 0
+    }
+  let multiplicacion = 1;
+  for (let i = 0; i < arguments.length; i++) {
+    multiplicacion = multiplicacion * arguments[i];
+  }
+    return multiplicacion;
 }
+
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  let contador = 0;
+  for (let i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] > 18) {
+      contador ++
+    }
+    }
+  return contador;
 }
 
 
@@ -129,7 +151,9 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if (numeroDeDia === 1 || numeroDeDia === 7) {
+    return "Es fin de semana"
+  } return "Es dia Laboral";
 } 
 
 
@@ -137,7 +161,10 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  let texto = n.toString;
+  if (texto[0] === 9 ) {
+    return true;
+  }return false;
 }
 
 
